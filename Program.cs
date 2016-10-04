ac3424e086f0ba7875e25fd13f7041411bf75eed
Bryan Sory <bryansory@icloud.com>
2016-10-03 22:17:35 -0500

    rudimentary inputs and outputs; no error checking

--- Program.cs
+++ Program.cs
@@ -11,17 +11,32 @@ namespace helloworld
         static void Main(string[] args)
         {
             string howdy = "Hello World";
-            int columbus = 1492;
-            //float myFloat = 5.45F;//
-            bool myDog = false;
+            Console.WriteLine(howdy);
+            Console.Write("When did Columbus sail?");
+            var columbus = Console.ReadLine();
+            Console.WriteLine("Columbus sailed the world in: " + columbus + ".");
+            Console.Write("Does Bryan have a dog? You must answer 'True or False'.");
+            var myDog = Console.ReadLine();
+            if  (myDog == "false")
+                {
+                    Console.WriteLine("It is " + myDog + " that I have a dog at home.");
+                }
+            else
+                {
+                     Console.WriteLine("It is " + myDog + " that I have a dog at home.");    
+                }
+            // bool myDog = false;
+            Console.Write("What is today's date?");
+            var userDate = Console.ReadLine();
             string date = DateTime.Today.ToShortDateString();
+            Console.WriteLine("WRONG, Segundo! Today is " + date );
             float myMoney = 5.45f;
             char myChar = 'X';
 
-            Console.WriteLine(howdy);
+            
             Console.ReadLine();
             Console.WriteLine("Columbus sailed the world in: " + columbus + ".");
-            Console.WriteLine("It is " + myDog + " that I have a dog at home.");
+            
             Console.WriteLine("Today is " + date + ".");
             Console.WriteLine("I spent $" + myMoney + " on a cheeseburger today.");
             Console.WriteLine(myChar + " marks the spot!");
