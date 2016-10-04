8110a23afc0ad964108349327eba55a22501c6b5
Bryan Sory <bryansory@icloud.com>
2016-10-03 19:59:52 -0500

    first program

--- Program.cs
+++ Program.cs
@@ -10,6 +10,25 @@ namespace helloworld
     {
         static void Main(string[] args)
         {
+            string howdy = "Hello World";
+            int columbus = 1492;
+            //float myFloat = 5.45F;//
+            bool myDog = false;
+            string date = DateTime.Today.ToShortDateString();
+            float myMoney = 5.45f;
+            char myChar = 'X';
+
+            Console.WriteLine(howdy);
+            Console.ReadLine();
+            Console.WriteLine("Columbus saild the world in: " + columbus);
+            Console.WriteLine("It is " + myDog + "that I have a dog at home");
+            Console.WriteLine("Today is " + date);
+            Console.WriteLine("I spent $" + myMoney + "on a cheeseburger today.");
+            Console.WriteLine(myChar + " marks the spot!");
         }
+    }
+    
+    internal class money
+    {
     }
 }
